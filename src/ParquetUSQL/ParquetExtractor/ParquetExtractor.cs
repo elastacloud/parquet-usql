2fd8a9eb9c48987f43185483c442cd932a653cf4
Richard Conway <richard@elastacloud.com>
2017-07-12 08:36:58 +0100

    first project checkin

---
+++ src/ParquetUSQL/ParquetExtractor/ParquetExtractor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ParquetExtractor
+{
+   public class ParquetExtractor : IExtractor
+   {
+      public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow output)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
