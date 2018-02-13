6be9bb035d1010c02270deb21944a45bb03b4794
Darren Fuller <darren@elastacloud.com>
2018-02-13 12:18:10 +0000

    remove redundant code and cleanup

--- src/Parquet.Adla/Outputter/ParquetOutputter.cs
+++ src/Parquet.Adla/Outputter/ParquetOutputter.cs
@@ -1,19 +1,11 @@
 ﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Types.Sql;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Parquet;
-using Parquet.Data;
 
 namespace Parquet.Adla.Outputter
 {
    [SqlUserDefinedOutputter(AtomicFileProcessing = true)]
    public class ParquetOutputter : IOutputter
    {
-      private DataSetBuilder _builder = new DataSetBuilder();
+      private readonly DataSetBuilder _builder = new DataSetBuilder();
 
       public override void Output(IRow input, IUnstructuredWriter output)
       {
